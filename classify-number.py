159e12212f3579eb3ae0d0b281b0aca33879aeb1
laban254 <labanrotich6544@gmail.com>
2025-02-03 15:02:14 +0300

    update allowed origins

--- classify-number.py
+++ classify-number.py
@@ -7,7 +7,7 @@ from fastapi.exceptions import RequestValidationError
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["GET"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
